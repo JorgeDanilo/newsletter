2b6f30409e2ae10df4ef81c1c5791ee8349c5668
JorgeDanilo <sobrejavatec@gmail.com>
2025-06-15 10:35:23 -0300

    refactor code using base fragment

--- app/src/main/java/com/jd/newsletter/ui/screen/home/NewsletterFragment.kt
+++ app/src/main/java/com/jd/newsletter/ui/screen/home/NewsletterFragment.kt
@@ -4,11 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.paging.LoadState
+import androidx.paging.PagingData
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.jd.domain.model.NewsModel
 import com.jd.newsletter.databinding.FragmentNewsletterBinding
 import com.jd.newsletter.ui.adapters.LoadingStateAdapter
 import com.jd.newsletter.ui.adapters.NewsletterAdapter
@@ -20,64 +21,73 @@ import io.reactivex.disposables.CompositeDisposable
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class NewsletterFragment : Fragment() {
+class NewsletterFragment : BaseFragment<FragmentNewsletterBinding>() {
 
     private val viewModel: NewsletterViewModel by viewModels()
     private val mAdapter by lazy { NewsletterAdapter() }
-    private lateinit var _binding: FragmentNewsletterBinding
     private val mDisposable = CompositeDisposable()
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentNewsletterBinding.inflate(inflater, container, false)
-        return _binding.root
+    override fun inflateBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentNewsletterBinding {
+        return FragmentNewsletterBinding.inflate(inflater, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel.getNewsLetter()
-        setupRecyclerView()
-        observer()
+        binding?.let {
+            viewModel.getNewsLetter()
+            setupRecyclerView(it)
+            observer(it)
+        }
     }
 
-    private fun setupRecyclerView() = with(_binding.rcvNewsletter) {
-        this.adapter = mAdapter
-        layoutManager = LinearLayoutManager(context)
-        this.adapter = mAdapter.withLoadStateFooter(
-            footer = LoadingStateAdapter()
-        )
+    private fun setupRecyclerView(binding: FragmentNewsletterBinding) {
+        binding.rcvNewsletter.let {
+            it.adapter = mAdapter
+            it.layoutManager = LinearLayoutManager(context)
+            it.adapter = mAdapter.withLoadStateFooter(
+                footer = LoadingStateAdapter()
+            )
+        }
     }
 
-    private fun observer() = lifecycleScope.launch {
+    private fun observer(binding: FragmentNewsletterBinding) = lifecycleScope.launch {
         viewModel.newsletter.collect { newsletter ->
-            _binding.apply {
-                when (newsletter) {
-                    is ResourceState.Success -> {
-                        newsletter.data?.let {
-                            tvEmptyList.hide()
+            handleNewsletterState(binding, newsletter)
+        }
+    }
 
-                            mAdapter.addLoadStateListener { loadState ->
-                                val errorState = loadState.source.append as? LoadState.Error
-                                    ?: loadState.source.prepend as? LoadState.Error
-                                    ?: loadState.append as? LoadState.Error
-                                    ?: loadState.prepend as? LoadState.Error
+    private fun handleNewsletterState(
+        binding: FragmentNewsletterBinding,
+        newsletter: ResourceState<PagingData<NewsModel>>
+    ) {
+        binding.apply {
+            when (newsletter) {
+                is ResourceState.Success -> {
+                    newsletter.data?.let {
+                        tvEmptyList.hide()
 
-                                errorState?.let {
-                                    tvEmptyList.show()
-                                }
+                        mAdapter.addLoadStateListener { loadState ->
+                            val errorState = loadState.source.append as? LoadState.Error
+                                ?: loadState.source.prepend as? LoadState.Error
+                                ?: loadState.append as? LoadState.Error
+                                ?: loadState.prepend as? LoadState.Error
+
+                            errorState?.let {
+                                tvEmptyList.show()
                             }
-                            mAdapter.submitData(lifecycle, it)
                         }
+                        mAdapter.submitData(lifecycle, it)
                     }
-
-                    is ResourceState.Error -> {
-                        tvEmptyList.show()
-                    }
-
-                    else -> {}
                 }
+
+                is ResourceState.Error -> {
+                    tvEmptyList.show()
+                }
+
+                else -> {}
             }
         }
     }
